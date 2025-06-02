853cd435c39cf1305c976e4e9bf1c8809fcf5ad6
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-06-02 16:02:24 -0700

    Remove AuthInfo export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@
 export { default as createMcpHandler } from "./next";
 
 export { withMcpAuth as experimental_withMcpAuth } from "./next/auth-wrapper";
-export type { AuthInfo } from "./next/auth-wrapper";
